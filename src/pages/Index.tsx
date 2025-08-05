cbd8c8c26c0b515e8b09ddcc9452602dbba0874a
[dyad] <gb_361810@akesp.net>
2025-08-06 00:26:22 +0500

    Reverted all changes back to version 959ba3abe3b675481475fcf17355a4d639e1058a

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,21 +1,18 @@
 import { AbgAnalyzer } from "@/components/AbgAnalyzer";
 import { ThemeToggle } from "@/components/ThemeToggle";
 import { MadeWithDyad } from "@/components/made-with-dyad";
-import VantaBackground from "@/components/VantaBackground";
 
 const Index = () => {
   return (
-    <VantaBackground>
-      <div className="flex flex-col items-center p-4">
-        <div className="absolute top-4 right-4">
-          <ThemeToggle />
-        </div>
-        <div className="w-full max-w-6xl mx-auto space-y-8 py-8">
-          <AbgAnalyzer />
-        </div>
-        <MadeWithDyad />
+    <div className="min-h-screen bg-gray-50 dark:bg-background flex flex-col items-center p-4">
+      <div className="absolute top-4 right-4">
+        <ThemeToggle />
       </div>
-    </VantaBackground>
+      <div className="w-full max-w-6xl mx-auto space-y-8 py-8">
+        <AbgAnalyzer />
+      </div>
+      <MadeWithDyad />
+    </div>
   );
 };
 

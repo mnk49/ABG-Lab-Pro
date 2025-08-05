d1abe5344c3accbc3dc300950a3bb169cd0e8670
[dyad] <gb_361810@akesp.net>
2025-08-06 00:43:14 +0500

    Reverted all changes back to version fc3e44f1e2da10f110de4b711e998387cd7414d1

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,12 +1,12 @@
 import { AbgAnalyzer } from "@/components/AbgAnalyzer";
-import { AccessibilityMenu } from "@/components/AccessibilityMenu";
+import { ThemeToggle } from "@/components/ThemeToggle";
 import { Footer } from "@/components/Footer";
 
 const Index = () => {
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-background flex flex-col">
       <div className="absolute top-6 right-6">
-        <AccessibilityMenu />
+        <ThemeToggle />
       </div>
       <main className="flex-grow w-full max-w-6xl mx-auto px-4 py-12">
         <AbgAnalyzer />
